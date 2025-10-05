9494bdef9b0d8acda208c8ed938db171bd86e3ba
Venkatesh krishnan <venkatesh.krishnan@sagaa.ai>
2025-10-05 09:29:46 -0700

    Switch to API key auth temporarily

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,16 +1,10 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-     }).authorization(allow => [allow.owner()]),
+     }).authorization(allow => [allow.publicApiKey()]), // Changed from allow.owner()
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -18,11 +12,9 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'userPool',
-    // API Key is used for a.allow.public() rules
+    defaultAuthorizationMode: 'apiKey', // Changed from 'userPool'
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
   },
 });
-
