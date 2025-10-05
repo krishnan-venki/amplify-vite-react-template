8f3c8f05ad8635e8a69341b7607c2a117ae68f61
Venkatesh krishnan <venkatesh.krishnan@sagaa.ai>
2025-10-05 09:53:36 -0700

    reenabled auth

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,10 +1,16 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
+/*== STEP 1 ===============================================================
+The section below creates a Todo database table with a "content" field. Try
+adding a new "isDone" field as a boolean. The authorization rule below
+specifies that any user authenticated via an API key can "create", "read",
+"update", and "delete" any "Todo" records.
+=========================================================================*/
 const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-     }).authorization(allow => [allow.publicApiKey()]), // Changed from allow.owner()
+     }).authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -12,9 +18,11 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'apiKey', // Changed from 'userPool'
+    defaultAuthorizationMode: 'userPool',
+    // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
   },
 });
+
